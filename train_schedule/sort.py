d36ad14f3e6ba8cece0bb4acfd0c9cf65f7d4748
Antoine Rozo <antoine.rozo@gmail.com>
2021-01-30 00:39:49 +0100

    Sort both by departure & arrival

--- train_schedule/sort.py
+++ train_schedule/sort.py
@@ -11,19 +11,25 @@ def diff_time(t1, t2):
 
 
 def compute_distances(trains):
-    distances = {} # stop -> (max_dist, n_stops, src)
+    distances_to = {} # stop -> (max_dist, n_stops, src)
+    distances_from = {}
     default = (0, 0, None)
 
     for train in trains:
         for (src, time_src), (dst, time_dst) in train.iter_parts():
             dist = diff_time(time_src, time_dst)
-            distances.setdefault(src, default)
-            old_dist, _, _ = distances.get(dst, default)
+            distances_to.setdefault(src, default)
+            distances_from.setdefault(dst, default)
 
+            old_dist, _, _ = distances_to.get(dst, default)
             if not old_dist or dist < old_dist:
-                distances[dst] = dist, 1, src
+                distances_to[dst] = dist, 1, src
 
-    return distances
+            old_dist, _, _ = distances_from.get(src, default)
+            if not old_dist or dist < old_dist:
+                distances_from[src] = dist, 1, dst
+
+    return distances_to, distances_from
 
 
 def normalize_distances(distances):
@@ -43,6 +49,13 @@ def normalize_distances(distances):
 
 
 def get_sorted_stops(trains):
-    distances = compute_distances(trains)
-    normalize_distances(distances)
-    return sorted(distances, key=distances.get)
+    distances_to, distances_from = compute_distances(trains)
+    normalize_distances(distances_to)
+    normalize_distances(distances_from)
+
+    def sort_key(key):
+        dist1, n1, _ = distances_to[key]
+        dist2, n2, _, = distances_from[key]
+        return dist1 - dist2, n1 - n2
+
+    return sorted(distances_to, key=sort_key)

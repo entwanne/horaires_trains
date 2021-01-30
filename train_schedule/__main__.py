e72549fc1c174b12b17a00e60203d0e30aec9ffb
Antoine Rozo <antoine.rozo@gmail.com>
2021-01-30 16:20:50 +0100

    Add option to unify days

--- train_schedule/__main__.py
+++ train_schedule/__main__.py
@@ -4,6 +4,7 @@ import sys
 from . import parse
 from .group import get_day_groups
 from .group import group_trains
+from .print import print_table
 from .print import print_day_tables
 from .sort import get_sorted_stops
 
@@ -13,6 +14,7 @@ def get_args():
     parser.add_argument('--input', '-i', type=argparse.FileType('r'), default=sys.stdin)
     parser.add_argument('--output', '-o', type=argparse.FileType('w'), default=sys.stdout)
     parser.add_argument('--format', '-f', choices=('md', 'html'), default=None)
+    parser.add_argument('--unify-days', '-u', action='store_true')
     args = parser.parse_args()
     if args.format is None:
         args.format = 'html' if args.output.name.endswith('.html') else 'md'
@@ -25,7 +27,10 @@ def main():
     day_groups = get_day_groups(trains)
     sorted_stops = get_sorted_stops(trains)
     grouped_trains = group_trains(trains, sorted_stops)
-    print_day_tables(sorted_stops, grouped_trains, day_groups, format=args.format, file=args.output)
+    if args.unify_days:
+        print_table(sorted_stops, grouped_trains, format=args.format, file=args.output)
+    else:
+        print_day_tables(sorted_stops, grouped_trains, day_groups, format=args.format, file=args.output)
 
 
 if __name__ == '__main__':
